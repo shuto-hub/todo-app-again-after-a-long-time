082719c85a7a14a49c0e86c764277ad165462680
shuto-hub <54265867+shuto-hub@users.noreply.github.com>
2025-08-28 13:37:17 +0900

    [fix]manifestファイルおよびcssファイルの名称修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link app.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
