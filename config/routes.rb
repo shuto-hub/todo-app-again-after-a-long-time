77089ea3872a7060fc170586815fe52a47d2daf5
shuto-hub <54265867+shuto-hub@users.noreply.github.com>
2025-08-29 17:33:39 +0900

    [add]リスト 14.27まで実施

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,16 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
+  resources :relationships,       only: [:create, :destroy]
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
